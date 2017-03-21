8ba4f2e7b5e0b7bf591fca288734b92ec1cd90ac
Laura Chen <linkinmonkey@gmail.com>
2017-03-21 19:17:24 -0400

    add script

---
+++ Assets/MoveJoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJoint : MonoBehaviour {
+
+	public Rigidbody theJoint;
+	public bool toUp = false;
+
+	void FixedUpdate()
+	{
+		if(toUp)
+		{
+			theJoint.AddForce ( new Vector3(0,10f,0), ForceMode.Impulse );
+			toUp = false;
+		}
+	}
+}
