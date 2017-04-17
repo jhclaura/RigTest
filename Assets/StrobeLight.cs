c3e90fbd80c7c891eccefee75798a4e7b0adee99
Laura Chen <linkinmonkey@gmail.com>
2017-04-17 18:54:08 -0400

    add shader ver strobe light but still haven't get the perfect feeling yet

--- Assets/StrobeLight.cs
+++ Assets/StrobeLight.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class StrobeLight : MonoBehaviour {
 
+	public enum MaskType
+	{
+		Mesh,
+		Shader
+	}
+	public MaskType maskType = MaskType.Shader;
+
 	public float flashPerSecond = 60f;
 	public bool strobeLightOn = false;
 
@@ -13,49 +20,97 @@ public class StrobeLight : MonoBehaviour {
 	private Color maskOff; //transparent
 	private float accumulatedTime = 0f;
 	private float timeInterval;
+	private bool inStrobeMode = false;
+	private bool inBlack = false;
 
 	void Start ()
 	{
-		renderer = GetComponent<Renderer> ();
+		switch(maskType)
+		{
+		case MaskType.Mesh:
+			renderer = GetComponent<Renderer> ();
+			maskOff = renderer.material.color;
+			maskOn = new Color (maskOff.a, maskOff.g, maskOff.b, 1f);
+			break;
 
-		maskOff = renderer.material.color;
-		maskOn = new Color (maskOff.a, maskOff.g, maskOff.b, 1f);
+		case MaskType.Shader:
+			break;
+		}
 
 	}
 	
 	void Update ()
 	{
-		if (strobeLightOn) {
+		if (strobeLightOn)
+		{
+			if(!inStrobeMode)
+				inStrobeMode = true;
+
 			timeInterval = 1f / flashPerSecond;
-
 			accumulatedTime += Time.deltaTime;
-			//Debug.Log (delay);
 
-			// off
-			if (accumulatedTime > timeInterval) {
-				renderer.material.color = maskOff;
+			if (accumulatedTime > timeInterval)
+			{
+				// light:1
+				if (renderer != null)
+					renderer.material.color = maskOff;
+				else
+					SteamVR_Fade.Start (Color.clear, 0);
 
 				accumulatedTime = 0f;
-			} else {
-				renderer.material.color = maskOn;
+				inBlack = true;
 			}
-		} else {
-			// reset
-			renderer.material.color = maskOff;
-			accumulatedTime = 0f;
+			else if(inBlack)
+			{
+				// light:0
+				if (renderer != null)
+					renderer.material.color = maskOff;
+				else
+					SteamVR_Fade.Start (Color.black, 0);
+
+				inBlack = false;
+			}
+		}
+		else if(inStrobeMode)
+		{
+			Debug.Log ("reset");
+			Reset ();
 		}
 	}
 
+	// === Public functions ===
 	public void TurnOnStrobeLight()
 	{
 		strobeLightOn = true;
+		inStrobeMode = true;
 	}
 
 	public void TurnOffStrobeLight()
 	{
 		// reset
 		strobeLightOn = false;
-		renderer.material.color = maskOff;
+
+
+		//renderer.material.color = maskOff;
+		//accumulatedTime = 0f;
+	}
+
+	private void Reset()
+	{
+		switch(maskType)
+		{
+		case MaskType.Mesh:
+			renderer.material.color = maskOff;
+			break;
+
+		case MaskType.Shader:
+			SteamVR_Fade.Start(Color.clear, 0);
+			break;
+		}
+
+		// universal ones
 		accumulatedTime = 0f;
+		inBlack = false;
+		inStrobeMode = false;
 	}
 }
